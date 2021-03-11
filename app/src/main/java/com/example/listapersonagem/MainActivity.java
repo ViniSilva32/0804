781ad99258eff1758699b4909a821ad0579d2e86
ViniSilva32 <78766084+ViniSilva32@users.noreply.github.com>
2021-03-11 20:17:43 -0300

    Aula demonstrando a construção de um sistema de listas mobile 2

---
+++ app/src/main/java/com/example/listapersonagem/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.listapersonagem;
+
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        List<String> personagem = new ArrayList<>(Arrays.asList("Alex","Ken","Ryu", "Chun-li"));
+
+        ListView listaDePersonagens = findViewById(R.id.activity_main_lista_personagem);
+        listaDePersonagens.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,personagem));
+
+
+
+
+       /* TextView primeiroPersonagem = findViewById(R.id.textView);
+        TextView segundoPersonagem = findViewById(R.id.textView2);
+        TextView terceiroPersonagem = findViewById(R.id.textView3);
+
+        primeiroPersonagem.setText(personagem.get(0));
+        segundoPersonagem.setText(personagem.get(1));
+        terceiroPersonagem.setText(personagem.get(2));*/
+
+    }
+
+}
---
+++ app/src/main/java/com/example/listapersonagem/ui/activities/FormularioPersonagemActivity.java
@@ -0,0 +1,16 @@
+package com.example.listapersonagem.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.listapersonagem.R;
+
+public class FormularioPersonagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_personagem);
+    }
+}
